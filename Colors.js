31c244449a112c60fb06cf9c79915e8f70e88552
Regina Reyes <82912274+Regina-28@users.noreply.github.com>
2024-11-08 15:10:24 -0600

    Add files via upload

---
+++ Colors.js
@@ -0,0 +1,13 @@
+import { Dimensions } from "react-native";
+const {width, height} = Dimensions.get("window");
+
+export const COLORES={
+    background:"#864D7E",
+    boton:"#AD7FA0",
+    errorText: "#351F32",
+
+    claro: "#8C5684",
+
+
+    circuloRutas: "#955A84",
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import {COLORES} from "./Colors";
+
+export {
+    COLORES
+};
